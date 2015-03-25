a9b316c2a9162b41da67197e6a471bee3d9878f6
Stefan Dasbach <StefanDasbach@gmail.com>
2015-03-25 17:43:27 -0400

    Allow user's state to be found and saved after login

--- Splat/Controllers/RootNavViewController.swift
+++ Splat/Controllers/RootNavViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Foundation
 import Parse
 
-class RootNavViewController: UINavigationController {
+class RootNavViewController: UINavigationController, CLLocationManagerDelegate {
     var bottomToolbar: UITabBar!
+    let locationManager = CLLocationManager()
     
     override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
@@ -45,7 +46,7 @@ class RootNavViewController: UINavigationController {
                 var test = PFUser.currentUser()
                 println(test.objectId)
                 
-                //remove old user defauls
+                //remove old user defaults
                 if let appDomain = NSBundle.mainBundle().bundleIdentifier {
                     NSUserDefaults.standardUserDefaults().removePersistentDomainForName(appDomain)
                     println(NSUserDefaults.standardUserDefaults().objectForKey("SplatUpvotes"))
@@ -56,6 +57,12 @@ class RootNavViewController: UINavigationController {
         
         self.view.backgroundColor = UIColor.whiteColor()
         self.navigationBar.barTintColor = UIColorFromRGB(PURPLE_SELECTED)
+
+        /* Get user location */
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        getUserLocation()
         // Do any additional setup after loading the view, typically from a nib.
         renderElements()
     
@@ -65,7 +72,45 @@ class RootNavViewController: UINavigationController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func getUserLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        /* Is user's location available? */
+        if let userLocation = locationManager.location {
+            saveUserLocation(userLocation)
+        }
+        else {
+            /* Calls didUpdateLocations when user's location is available */
+            locationManager.startUpdatingLocation()
+        }
+    }
+    /* Waits for user's location to become available */
+    func locationManager(manager: CLLocationManager!, didUpdateLocations
+        locations: [AnyObject]!) {
+            if let userLocation = manager.location {
+                saveUserLocation(userLocation)
+            }
+            manager.stopUpdatingLocation()
+    }
+    func saveUserLocation(userLocation: CLLocation) {
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks: [AnyObject]!, error: NSError!) in
+            if error == nil && placemarks.count > 0 {
+                let geoLocation = placemarks[0] as CLPlacemark
+                let country = geoLocation.country
+                let state = geoLocation.administrativeArea
+                let defaults = NSUserDefaults.standardUserDefaults()
+                if country == "United States" {
+                    defaults.setObject(state, forKey: "state")
+                }
+                else {
+                    defaults.setObject(state, forKey: "foreign")
+                }
+            }
+        })
+    }
+    
     func renderElements() {
         //may re-add this in the future
         
