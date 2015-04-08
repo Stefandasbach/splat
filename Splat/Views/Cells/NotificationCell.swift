6b44e8637ca163776a12f7119703f1dcf17245e9
Aaron Tainter <atainter@iastate.edu>
2015-04-08 00:23:02 -0500

    finished notifications

--- Splat/Views/Cells/NotificationCell.swift
+++ Splat/Views/Cells/NotificationCell.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 class NotificationCell: UITableViewCell {
+    let UIPadding: CGFloat = 10
+    let imageSize: CGFloat = 30
+    
+    var postPicture: UIImageView!
+    private var notificationTime: UILabel!
+    private var notificationText: UILabel!
+    
+    private var currentNotification: Notification!
     
     //this is an error workaround...
     let cellWidth = UIScreen.mainScreen().applicationFrame.width
@@ -24,5 +32,48 @@ class NotificationCell: UITableViewCell {
     }
     
     func renderCell() {
+        postPicture = UIImageView(frame: CGRectMake(cellWidth - imageSize - UIPadding, UIPadding, imageSize, imageSize))
+        postPicture.contentMode = UIViewContentMode.ScaleAspectFill
+        postPicture.clipsToBounds = true
+        postPicture.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.5)
+        
+        notificationText = UILabel(frame: CGRectMake(UIPadding, 0, 200, 50))
+        notificationText.text = "Someone replied to your post."
+        notificationText.font = UIFont.systemFontOfSize(12)
+        
+        notificationTime = UILabel(frame:  CGRectMake(notificationText.frame.maxX, 0, 50, 50))
+        notificationTime.center.y = notificationText.center.y
+        notificationTime.font = UIFont.systemFontOfSize(12)
+        notificationTime.textColor = UIColor.grayColor()
+        
+        
+        self.addSubview(postPicture)
+        self.addSubview(notificationText)
+        self.addSubview(notificationTime)
+    }
+    
+    func initialize(notification: Notification) {
+        currentNotification = notification
+        
+        var eventCreatedDate = notification.object.createdAt
+        var today = NSDate()
+        
+        let timeSincePost = getStringTimeDiff(eventCreatedDate, today)
+        notificationTime.text = "\(timeSincePost.number)\(timeSincePost.unit)"
+        
+        postPicture.image = nil
+        if let post = notification.getPost()? {
+            post.getEventPicture { (imageData) -> Void in
+                self.postPicture.image = UIImage(data: imageData)
+            }
+        }
+
+        
+    }
+    
+    func cancelLoad() {
+        if let post = currentNotification.getPost()? {
+            post.cancelPictureGet()
+        }
     }
 }
